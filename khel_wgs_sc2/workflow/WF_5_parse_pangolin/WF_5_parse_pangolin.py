70290fbad0f0544807620bd79c37166203796841
jbarnell1 <jonathanbarnell@gmail.com>
2022-02-28 09:13:23 -0600

    removing ssh for WF 5

--- khel_wgs_sc2/workflow/WF_5_parse_pangolin/WF_5_parse_pangolin.py
+++ khel_wgs_sc2/workflow/WF_5_parse_pangolin/WF_5_parse_pangolin.py
@@ -24,14 +24,7 @@ def run_script_5(run_id):
         compiled_fasta_path = data_obj.get_fasta_path()
         target_folders = compiled_fasta_path.split("/")
     target_folder = "/".join(target_folders[:-1])
-    try:
-        data_obj.send_fasta(compiled_fasta_path)
-        data_obj.run_pangolin()
-        data_obj.receive_pangolin_df(target_folder)
-        data_obj.clean_connections()
-    except Exception as e:
-        data_obj.clean_connections()
-        raise ValueError("Possible Connection error: " + e)
+    data_obj.run_pangolin()
     data_obj.get_pango_dfs(pango_path=target_folder + "/results.csv")
 
 
